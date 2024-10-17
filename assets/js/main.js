c9da7b0396ecf879b2c5d5ca6445ec682bb85968
Kelvin Bett <bettkelvin05@gmail.com>
2024-10-17 14:38:27 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+//get the button element
+
+const javaclass=document.getElementById('colorButton')
+
+//add even listener for the button click
+
+javaclass.addEventListener('click',()=>{
+    //change the color of the button
+
+    javaclass.style.backgroundColor=javaclass.style.backgroundColor==='red' ? '#4CAF50':'red';
+});
